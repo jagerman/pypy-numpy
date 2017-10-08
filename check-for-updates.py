246f8ea17ddc926a69f30633cd5c9df8ce27fc1c
Jason Rhinelander <jason@imaginary.ca>
2017-10-08 10:21:12 -0400

    only build stable version; multiple pypy versions
    This fixes the pypi version parsing to only look for stable versions
    (recently the update script has been failing to build scipy because it
    picks up the 1.0.0b1 and rc1 versions, but a build doesn't actually
    build those).
    
    It also adds support for building wheels for multiple pypy versions, and
    adds a 5.9.0 build.

--- check-for-updates.py
+++ check-for-updates.py
@@ -5,22 +5,43 @@ import requests
 import os.path
 import shutil
 from subprocess import run
-from config import pypy, pypy_wheel_suffix, repo
+from config import pypy, repo
+
+try:
+    from packaging.version import parse
+except ImportError:
+    from pip._vendor.packaging.version import parse
 
 def version(package_name):
     url = "https://pypi.python.org/pypi/{}/json".format(package_name)
-    data = requests.get(url).json()
-    return data["info"]["version"]
+    req = requests.get(url)
+    data = req.json()
+    version = parse('0')
+    found = False
+    if req.status_code == requests.codes.ok:
+        if 'releases' in data:
+            releases = data['releases']
+            for release in data['releases']:
+                ver = parse(release)
+                if not ver.is_prerelease:
+                    version = max(version, ver)
+                    found = True
+
+    if not found:
+        raise RuntimeError("Could not find latest stable version of {}".format(package_name))
+    return str(version)
 
 vnp, vsp = version("numpy"), version("scipy")
-np_whl = 'numpy-' + vnp + pypy_wheel_suffix
-sp_whl = 'scipy-' + vsp + pypy_wheel_suffix
-if not os.path.isfile(repo + '/numpy/' + np_whl) or not os.path.isfile(repo + '/scipy/' + sp_whl):
-    print("Numpy and/or scipy wheels outdated or do not exist; building new wheels")
-    run([repo + "/../build-numpy-pypy.sh", pypy], check=True)
-    if not os.path.isfile(repo + '/../' + np_whl) or not os.path.isfile(repo + '/../' + sp_whl):
-        raise RuntimeError("Build succeeded, but expected wheels not found!")
-    shutil.move(repo + "/../" + np_whl, repo + "/numpy/" + np_whl)
-    shutil.move(repo + "/../" + sp_whl, repo + "/scipy/" + sp_whl)
-    print("Generated new " + np_whl + " and " + sp_whl + " pypy wheels")
 
+for pypy_v, pypy_wheel_suffix in pypy.items():
+    np_whl = 'numpy-' + vnp + pypy_wheel_suffix
+    sp_whl = 'scipy-' + vsp + pypy_wheel_suffix
+    if not os.path.isfile(repo + '/numpy/' + np_whl) or not os.path.isfile(repo + '/scipy/' + sp_whl):
+        print("Numpy and/or scipy wheels outdated or do not exist; building new wheels")
+        print("Found current versions: numpy-{}, scipy-{}".format(vnp, vsp))
+        run([repo + "/../build-numpy-pypy.sh", pypy_v], check=True)
+        if not os.path.isfile(repo + '/../' + np_whl) or not os.path.isfile(repo + '/../' + sp_whl):
+            raise RuntimeError("Build succeeded, but expected wheels not found!")
+        shutil.move(repo + "/../" + np_whl, repo + "/numpy/" + np_whl)
+        shutil.move(repo + "/../" + sp_whl, repo + "/scipy/" + sp_whl)
+        print("Generated new " + np_whl + " and " + sp_whl + " pypy wheels")
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # Where to move the built wheels, i.e. the web-accessible PyPI repository
 repo = 'trusty-pypi'
-# PyPy binary version
-pypy = 'pypy2-v5.8.0-linux64'
-# The expected pypy wheel suffix
-pypy_wheel_suffix = '-pp258-pypy_41-linux_x86_64.whl'
+# pypy binary versions (keys) and pypy wheel suffixes (values) for each pypy
+# version to build for:
+pypy = {
+    'pypy2-v5.8.0-linux64': '-pp258-pypy_41-linux_x86_64.whl',
+    'pypy2-v5.9.0-linux64': '-pp259-pypy_41-linux_x86_64.whl'
+}
